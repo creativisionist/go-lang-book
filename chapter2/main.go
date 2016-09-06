6e4a6b4cfed4ec51e6446d0e162cca12abb8d8dd
creativisionist <creativisionist@gmail.com>
2016-09-06 13:56:03 -0500

    Initial commit.

---
+++ chapter2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// this is a comment
+
+func main() {
+  fmt.Println("Hello, my name is computer.")
+  fmt.Println(len("Hello World"))
+  fmt.Println("Hello World"[1])
+  fmt.Println("Hello " + "World")
+  var x string = "Hello World"
+  fmt.Println(x)
+  y := 5
+  fmt.Println(y)
+}
