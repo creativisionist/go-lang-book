e35f9c9e8d2be682865d1b4d9c6a769935baa2ab
creativisionist <creativisionist@gmail.com>
2016-09-08 10:41:38 -0500

    Add comments.

--- chapter8/pointers.go
+++ chapter8/pointers.go
@@ -23,3 +23,20 @@ func main() {
   zero(&x)
   fmt.Println(x) // x is 0
 }
+
+// Pointer is represented using the * (asterisk)
+// character followed by the type of the stored value. In
+// the zero function xPtr is a pointer to an int.
+// * is also used to “dereference” pointer variables. Dereferencing
+// a pointer gives us access to the value the
+// pointer points to. When we write *xPtr = 0 we are saying
+// “store the int 0 in the memory location xPtr refers
+// to”. If we try xPtr = 0 instead we will get a compiler
+// error because xPtr is not an int it's a *int, which can
+// only be given another *int.
+// Finally we use the & operator to find the address of a
+// Pointers 94
+// variable. &x returns a *int (pointer to an int) because x
+// is an int. This is what allows us to modify the original
+// variable. &x in main and xPtr in zero refer to the same
+// memory location.
