602f38dbb19e57cb213448c06fd5365220cad1b8
creativisionist <creativisionist@gmail.com>
2016-09-08 10:58:21 -0500

    Add 'new' function.

--- chapter8/pointers.go
+++ chapter8/pointers.go
@@ -15,14 +15,14 @@ import "fmt"
 // is stored rather than the value itself. (They point to
 // something else) By using a pointer (*int) the zero
 // function is able to modify the original variable.
-func zero(xPtr * int) {
-  *xPtr = 0
-}
-func main() {
-  x := 5
-  zero(&x)
-  fmt.Println(x) // x is 0
-}
+// func zero(xPtr * int) {
+//   *xPtr = 0
+// }
+// func main() {
+//   x := 5
+//   zero(&x)
+//   fmt.Println(x) // x is 0
+// }
 
 // Pointer is represented using the * (asterisk)
 // character followed by the type of the stored value. In
@@ -40,3 +40,17 @@ func main() {
 // is an int. This is what allows us to modify the original
 // variable. &x in main and xPtr in zero refer to the same
 // memory location.
+
+
+// New takes a type as an argument, allocates enough
+// memory to fit a value of that type and returns a
+// pointer to it.
+// Note: Garbage collected
+func one(xPtr *int) {
+  *xPtr = 1
+}
+func main() {
+  xPtr := new(int)
+  one(xPtr)
+  fmt.Println(*xPtr) // x is 1
+}
