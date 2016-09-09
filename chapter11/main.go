0a5ca022b31dc358b631518e98f888a683ec6354
creativisionist <creativisionist@gmail.com>
2016-09-09 14:36:27 -0500

    Install go-pry.  Rm alias for it to work.

--- chapter11/main.go
+++ chapter11/main.go
@@ -1,10 +1,12 @@
 package main
 
 import "fmt"
-import  m "github.com/golang-book/chapter11/math"
+import  "github.com/golang-book/chapter11/math"
+import "github.com/d4l3k/go-pry/pry"
 
 func main() {
   xs := []float64{1,2,3,4}
-  avg := m.Average(xs)
+  avg := math.Average(xs)
+  pry.Pry()
   fmt.Println(avg)
 }
