fc2aeda3a30fb7e5f4d37fd93f9e15f094715c3e
creativisionist <creativisionist@gmail.com>
2016-09-12 15:47:19 -0500

    Add sort by age.

--- chapter13/sort.go
+++ chapter13/sort.go
@@ -10,23 +10,44 @@ type Person struct {
   Age int
 }
 
-type ByName []Person
+// type ByName []Person
 
-func (this ByName) Len() int {
+// func (this ByName) Len() int {
+//   return len(this)
+// }
+// func (this ByName) Less(i, j int) bool {
+//   return this[i].Name < this[j].Name
+// }
+// func (this ByName) Swap(i, j int) {
+//   this[i], this[j] = this[j], this[i]
+// }
+
+// func main() {
+//   kids := []Person{
+//     {"Jill",9},
+//     {"Jack",10},
+//   }
+//   sort.Sort(ByName(kids))
+//   fmt.Println(kids)
+// }
+
+type ByAge []Person
+func (this ByAge) Len() int {
   return len(this)
 }
-func (this ByName) Less(i, j int) bool {
-  return this[i].Name < this[j].Name
+func (this ByAge) Less(i, j int) bool {
+  return this[i].Age < this[j].Age
 }
-func (this ByName) Swap(i, j int) {
+func (this ByAge) Swap(i, j int) {
   this[i], this[j] = this[j], this[i]
 }
 
 func main() {
-  kids := []Person{
-    {"Jill",9},
-    {"Jack",10},
+  simpsons := []Person{
+    {"Maggie", 2},
+    {"Bart", 10},
+    {"Lisa", 8},
   }
-  sort.Sort(ByName(kids))
-  fmt.Println(kids)
+  sort.Sort(ByAge(simpsons))
+  fmt.Println(simpsons)
 }
